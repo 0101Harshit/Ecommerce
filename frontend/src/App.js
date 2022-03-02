116a0b0ff0c5a197f9ac879a421dfc4929670347
0101Harshit <01harshitsoni@gmail.com>
2022-03-02 15:59:53 +0530

    homescreen- list products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import data from "./data";
+
 function App() {
   return (
     <div>
@@ -5,7 +8,28 @@ function App() {
         <a href="/"> Ecommerce </a>
       </header>
       <main>
-        List of products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product, index) => {
+            return (
+              <div className="product" key={index}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
+              
+              </div>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
