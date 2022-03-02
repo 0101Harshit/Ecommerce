b9b39f54ecb6a03f1053360a26e4e3b81233a59c
0101Harshit <01harshitsoni@gmail.com>
2022-03-02 12:17:22 +0530

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> Ecommerce </a>
+      </header>
+      <main>
+        List of products
+      </main>
+    </div>
+  );
+}
+
+export default App;
