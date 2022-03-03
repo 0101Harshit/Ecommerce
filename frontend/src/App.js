65f72e1f7aafdebf593d53e8531fc69b8c4b95fb
0101Harshit <01harshitsoni@gmail.com>
2022-03-03 17:28:16 +0530

    add react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,35 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
+    <div className="d-flex flex-column site-container">
       <header>
-        <Link to="/"> Ecommerce </Link>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>Ecommerce</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
       </header>
       <main>
+        <Container>
         <Routes>
           <Route path="/" element={<HomeScreen />} />
           <Route path="/product/:slug" element={<ProductScreen />} />
         </Routes>
+        </Container>
       </main>
+      <footer className="text-center">
+        <div>All right reserved</div>
+      </footer>
+      </div>
     </BrowserRouter>
   );
 }
